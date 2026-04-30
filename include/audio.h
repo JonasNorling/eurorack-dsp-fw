d7a1c453ea42a2f67f873a7f1aca57d852d1e772
Jonas Norling <jonas@elemental.se>
2026-04-30 08:34:09 +0200

    Fix saturation bug

--- include/audio.h
+++ include/audio.h
@@ -4,8 +4,8 @@
 #define FRAMES_PER_BLOCK 2
 #define SAMPLE_RATE 48000
 #define MS_PER_FRAME (1000.0f * FRAMES_PER_BLOCK / SAMPLE_RATE)
-#define SAMPLE_MAX (1 << 23)
-#define SAMPLE_MIN (-(1 << 23) + 1)
+#define SAMPLE_MAX ((1 << 23) - 1)
+#define SAMPLE_MIN (-(1 << 23))
 
 typedef struct {
     float s[2];  // left & right channel
